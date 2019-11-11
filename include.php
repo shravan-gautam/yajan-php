88d4df313af5595472b8e4a4d6d616aa74462008
shravan <shravan@devl.awgp.in>
2019-11-10 20:35:49 -0800

    yajan installation issue fix

--- include.php
+++ include.php
@@ -4,7 +4,7 @@
 $libVersion = "11.0";
 $bootstrapVersion = "3";
 $databoxVersion = "1.1";
-$FREAMWORK_PATH="yajan-php";
+$FREAMWORK_PATH="yajan";
 
 //////////////////////      BOOT LOADER            ////////////////////////////
 include "$FREAMWORK_PATH/lib/$libVersion/boot.php";
